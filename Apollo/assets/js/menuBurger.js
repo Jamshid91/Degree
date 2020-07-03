001d9bb794a9bbc64e3d7b2fb7ccaec6a17b55d6
Jamshid91 <jamshid_tojiboyev@mail.ru>
2020-07-03 17:38:44 +0500

    removed do not have codes

--- Apollo/assets/js/menuBurger.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('.menu-burger');
-let sub = document.querySelector('.sub-menu');
-
-menu.addEventListener('click', toggle)
-function toggle() {
-  sub.classList.toggle('hidden')
-}
